612ac891de0f5dfe7ff5bb81b7bb6ebac1a5615a
Elvir Dzeko <elvir@woocode.com>
2019-05-12 20:10:47 +0200

    Change logo, update code, fix focus issue and remove duplicate stuff

--- BorasGrappling2/MainPage.xaml.cs
+++ BorasGrappling2/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Navigation;
 
 namespace BorasGrappling2
@@ -9,6 +10,7 @@ namespace BorasGrappling2
     public sealed partial class MainPage : Page
     {
         private readonly DispatcherTimer timer;
+        private bool isOnRoundTime = true;
         private int secondsLeft;
         private int secondsRestLeft;
         private int minutesLeft;
@@ -19,16 +21,17 @@ namespace BorasGrappling2
         private float volume = 0.9f;
         private int secondsRestDefault;
 
+        private int exitCounter;
 
         public MainPage()
         {
             InitializeComponent();
             NavigationCacheMode = NavigationCacheMode.Required;
 
-
             timer = new DispatcherTimer {Interval = new TimeSpan(0, 0, 1)};
             timer.Tick += timer_Tick;
 
+            exitCounter = 0;
             secondsLeft = 0;
             minutesLeft = 5;
             minutesRestLeft = 0;
@@ -37,6 +40,85 @@ namespace BorasGrappling2
             roundCounter.Text = "1";
             timeLeft.Text = FormatTime(minutesLeft, secondsLeft);
             restTime.Text = FormatTime(minutesRestLeft, secondsRestLeft);
+
+            btnStart.Focus(FocusState.Keyboard);
+        }
+
+        private void BtnIncreaseTime_Click(object sender, RoutedEventArgs e)
+        {
+            IncreaseRoundTime();
+        }
+
+        private void BtnDecreaseTime_Click(object sender, RoutedEventArgs e)
+        {
+            DecreaseRoundTime();
+        }
+
+        protected override void OnLostFocus(RoutedEventArgs e)
+        {
+            base.OnLostFocus(e);
+
+            if (btnStart.FocusState == FocusState.Unfocused)
+                btnStart.Focus(FocusState.Programmatic);
+        }
+
+        protected override void OnKeyDown(KeyRoutedEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            if (e.Key == VirtualKey.Home)
+            {
+                exitCounter++;
+                if (exitCounter >= 3)
+                    ShutdownManager.BeginShutdown(ShutdownKind.Shutdown, TimeSpan.FromSeconds(0));
+            }
+
+            if (e.Key == VirtualKey.Up)
+                isOnRoundTime = true;
+
+            if (e.Key == VirtualKey.Down)
+                isOnRoundTime = false;
+
+            if (e.Key == VirtualKey.Enter)
+                StartOrPauseTimer();
+
+            if (btnDecreaseTime.IsEnabled && e.Key == VirtualKey.Left && isOnRoundTime)
+                DecreaseRoundTime();
+
+            if (btnDecreaseRestTime.IsEnabled && e.Key == VirtualKey.Left && !isOnRoundTime)
+                DecreaseRestTime();
+
+            if (btnIncreaseTime.IsEnabled && e.Key == VirtualKey.Right && isOnRoundTime)
+                IncreaseRoundTime();
+
+            if (btnIncreaseRestTime.IsEnabled && e.Key == VirtualKey.Right && !isOnRoundTime)
+                IncreaseRestTime();
+
+            if (e.Key == VirtualKey.Application)
+                ResetTimer();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            StartOrPauseTimer();
+        }
+
+        private static string FormatTime(int minutes, int seconds)
+        {
+            string minStr = minutes.ToString();
+            string secStr = seconds.ToString();
+
+            if(minutes < 10)
+            {
+                minStr = $"0{minutes}";
+            } 
+
+            if(seconds < 10)
+            {
+                secStr = $"0{seconds}";
+            }
+
+            return $"{minStr}:{secStr}";
         }
 
         void timer_Tick(object sender, object e)
@@ -79,7 +161,7 @@ namespace BorasGrappling2
                 //btnStart.Content = "START";
                 restTime.Text = FormatTime(minutesRestLeft, secondsRestLeft);
             }
-            else if(secondsLeft == 0 && minutesLeft != 0)
+            else if (secondsLeft == 0 && minutesLeft != 0)
             {
                 // decrease minutes and reset seconds to 59
                 minutesLeft = minutesLeft - 1;
@@ -88,34 +170,78 @@ namespace BorasGrappling2
             else
             {
                 secondsLeft = secondsLeft - 1;
-
             }
         }
 
-        private void BtnIncreaseTime_Click(object sender, RoutedEventArgs e)
+        private void ResetTimer()
         {
+            timer.Stop();
+            btnStart.Content = "START";
 
-            if(secondsLeft >= 30)
-            {
-                minutesLeft += 1;
-                secondsLeft = 0;
-            }
-            else
-            {
-                secondsLeft += 30;
-            }
+            exitCounter = 0;
+            secondsLeft = 0;
+            minutesLeft = 5;
+            minutesRestLeft = 0;
+            secondsRestLeft = 30;
 
+            btnIncreaseTime.IsEnabled = true;
+            btnDecreaseTime.IsEnabled = true;
+            btnIncreaseRestTime.IsEnabled = true;
+            btnDecreaseRestTime.IsEnabled = true;
+
+            roundCounter.Text = "1";
             timeLeft.Text = FormatTime(minutesLeft, secondsLeft);
+            restTime.Text = FormatTime(minutesRestLeft, secondsRestLeft);
+        }
 
-            // if the timer was at 0 make the button available again when there
-            // is more than 10 seconds
-            if (secondsLeft >= 30)
+        private void StartOrPauseTimer()
+        {
+            btnIncreaseTime.IsEnabled = false;
+            btnDecreaseTime.IsEnabled = false;
+            btnIncreaseRestTime.IsEnabled = false;
+            btnDecreaseRestTime.IsEnabled = false;
+
+            var startBtnText = btnStart.Content?.ToString();
+
+            // Do this only if there is timeleft and start is displayed!
+            if (startBtnText == "START" && (secondsLeft != 0 || minutesLeft != 0))
             {
-                btnDecreaseTime.IsEnabled = true;
+                PlaySoundAsync("startbell.wav");
+
+                secondsDefault = secondsLeft;
+                minutesDefault = minutesLeft;
+                secondsRestDefault = secondsRestLeft;
+                timer.Start();
+                btnStart.Content = "PAUSE";
+
+            }
+            else if (startBtnText == "PAUSE")
+            {
+                timer.Stop();
+                btnStart.Content = "CONTINUE";
+
+            }
+            else if (startBtnText == "CONTINUE")
+            {
+                timer.Start();
+                btnStart.Content = "PAUSE";
             }
         }
 
-        private void BtnDecreaseTime_Click(object sender, RoutedEventArgs e)
+        private async void PlaySoundAsync(string soundfile)
+        {
+            var mediaElement = new MediaElement();
+
+            var folder = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync("Assets");
+            var file = await folder.GetFileAsync(soundfile);
+            var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+
+            mediaElement.SetSource(stream, file.ContentType);
+            mediaElement.Volume = volume;
+            mediaElement.Play();
+        }
+
+        private void DecreaseRoundTime()
         {
             // We dont like negative rounds
             if (secondsLeft == 0 && minutesLeft == 0)
@@ -137,59 +263,29 @@ namespace BorasGrappling2
             timeLeft.Text = FormatTime(minutesLeft, secondsLeft);
         }
 
-        private void btnStart_Click(object sender, RoutedEventArgs e)
+        private void IncreaseRoundTime()
         {
-
-            btnIncreaseTime.IsEnabled = false;
-            btnDecreaseTime.IsEnabled = false;
-            btnIncreaseRestTime.IsEnabled = false;
-            btnDecreaseRestTime.IsEnabled = false;
-
-            var startBtnText = btnStart.Content?.ToString();
-
-            // Do this only if there is timeleft and start is displayed!
-            if (startBtnText == "START" && (secondsLeft != 0 || minutesLeft != 0))
+            if (secondsLeft >= 30)
             {
-                PlaySoundAsync("startbell.wav");
-
-                secondsDefault = secondsLeft;
-                minutesDefault = minutesLeft;
-                secondsRestDefault = secondsRestLeft;
-                timer.Start();
-                btnStart.Content = "PAUSE";
-
-            } else if (startBtnText == "PAUSE")
+                minutesLeft += 1;
+                secondsLeft = 0;
+            }
+            else
             {
-                timer.Stop();
-                btnStart.Content = "CONTINUE";
-
-            } else if (startBtnText == "CONTINUE")
-            {
-                timer.Start();
-                btnStart.Content = "PAUSE";
+                secondsLeft += 30;
             }
 
-        }
+            timeLeft.Text = FormatTime(minutesLeft, secondsLeft);
 
-        private static string FormatTime(int minutes, int seconds)
-        {
-            string minStr = minutes.ToString();
-            string secStr = seconds.ToString();
-
-            if(minutes < 10)
+            // if the timer was at 0 make the button available again when there
+            // is more than 10 seconds
+            if (secondsLeft >= 30)
             {
-                minStr = $"0{minutes}";
-            } 
-
-            if(seconds < 10)
-            {
-                secStr = $"0{seconds}";
+                btnDecreaseTime.IsEnabled = true;
             }
-
-            return $"{minStr}:{secStr}";
         }
 
-        private void BtnIncreaseRestTime_Click(object sender, RoutedEventArgs e)
+        private void IncreaseRestTime()
         {
             if (secondsRestLeft == 50)
             {
@@ -211,7 +307,8 @@ namespace BorasGrappling2
                 return;
             }
         }
-        private void BtnDecreaseRestTime_Click(object sender, RoutedEventArgs e)
+
+        private void DecreaseRestTime()
         {
             // We dont like negative rest
             if (secondsRestLeft == 0 && minutesRestLeft == 0)
@@ -233,49 +330,9 @@ namespace BorasGrappling2
             restTime.Text = FormatTime(minutesRestLeft, secondsRestLeft);
         }
 
-        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        private void BtnStart_Loaded(object sender, RoutedEventArgs e)
         {
-            //CoreApplication.Exit();
-            // replaced in package manifest:
-            // IgnorableNamespaces="uap mp"
-            // Shutdowns the device immediately:
-
-            // shutdown command: shutdown / s / t 0
-
-            ShutdownManager.BeginShutdown(ShutdownKind.Shutdown, TimeSpan.FromSeconds(0));
-        }
-
-        private void BtnReset_Click(object sender, RoutedEventArgs e)
-        {
-            timer.Stop();
-            btnStart.Content = "START";
-
-            secondsLeft = 0;
-            minutesLeft = 5;
-            minutesRestLeft = 0;
-            secondsRestLeft = 30;
-
-            btnIncreaseTime.IsEnabled = true;
-            btnDecreaseTime.IsEnabled = true;
-            btnIncreaseRestTime.IsEnabled = true;
-            btnDecreaseRestTime.IsEnabled = true;
-
-            roundCounter.Text = "1";
-            timeLeft.Text = FormatTime(minutesLeft, secondsLeft);
-            restTime.Text = FormatTime(minutesRestLeft, secondsRestLeft);
-        }
-
-        private async void PlaySoundAsync(string soundfile)
-        {
-            var mediaElement = new MediaElement();
-
-            var folder = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync("Assets");
-            var file = await folder.GetFileAsync(soundfile);
-            var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
-
-            mediaElement.SetSource(stream, file.ContentType);
-            mediaElement.Volume = volume;
-            mediaElement.Play();
+            btnStart.Focus(FocusState.Programmatic);
         }
     }
 }
